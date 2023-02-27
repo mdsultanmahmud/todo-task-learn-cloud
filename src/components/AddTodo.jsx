3193f6a22c97e4bf951ca3a8b7192e577964163d
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-27 16:48:56 +0600

    add task section done

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import { BiImageAdd } from 'react-icons/bi'
 const AddTodo = () => {
     return (
-        <div>
-            <h1 className='text-xl text-green-500'>Add Your Task</h1>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <form>
-                <input type="text" placeholder='Enter Your Task Name' />
-                <button type='submit'>Add</button>
+        <div className='my-20 mx-20 px-20'>
+            <h1 className='text-2xl text-[#FF5733] text-center font-bold mb-6'>Add Your Task</h1>
+            <form className='text-center'>
+                <div className='flex gap-5 items-center justify-center'>
+                    <input className='px-4 py-3 outline-none border border-[#FF5733] mr-4 rounded' type="text" placeholder='Enter Your Task Name' />
+                    <label htmlFor="file-upload" className='cursor-pointer'>
+                        <div className='flex items-center shadow-lg p-3'>
+                            <BiImageAdd size={22}/>
+                            <span className='font-bold ml-2'>Upload Task Icon</span>
+                        </div>
+                        <input type="file" id="file-upload" hidden /></label>
+                    <br />
+                </div>
+                <button className='bg-[#FF5733] px-4 py-3 rounded text-white text-xl font-bold w-1/2 mt-2' type='submit'>Add Task</button>
             </form>
         </div>
     );
--- src/components/DisplayTodo.jsx
+++ src/components/DisplayTodo.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DisplayTodo = () => {
     return (
-        <div>
-            <h1>Dis play all todo</h1>
+        <div className='mx-20 px-20'>
+            <div className='text-center'>
+                <button className='px-8 py-4 font-bold bg-[#FF5733] hover:bg-[#6495ED] text-white mr-4'>All Task</button>
+                <button className='px-8 py-4 font-bold bg-[#FF5733] hover:bg-[#6495ED] text-white'>Done Task</button>
+            </div>
         </div>
     );
 };
