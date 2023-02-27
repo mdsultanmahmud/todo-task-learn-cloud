e4cd767fdcee42ec8b6ee67dcb96cc4d0f9eeb74
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-27 16:11:44 +0600

    add tailwind and daisy ui

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const AddTodo = () => {
     return (
         <div>
-            <h1>Add Your Task</h1>
+            <h1 className='text-xl text-green-500'>Add Your Task</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
             <form>
-                <input type="text" placeholder='Enter Your Task Name'/>
+                <input type="text" placeholder='Enter Your Task Name' />
                 <button type='submit'>Add</button>
             </form>
         </div>
